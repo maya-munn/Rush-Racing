00d8b0da8982e5c70de9875dd2faeacc0253b4bc
tyrelljacks <64449374+tyrelljacks@users.noreply.github.com>
2020-05-05 22:30:16 +1200

    button updatw
    funtion for some buttons

---
+++ Rush Racing/Assets/Scripts/Menu ui/menuplay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class menuplay : MonoBehaviour
+{
+   
+    public void sceneLoader(int SceneMenu)
+    {
+        SceneManager.LoadScene(SceneMenu);
+    }
+}
