5c5c8f162cdafd57419577f71bd9f89a64be2d62
Lê Trung Hải <letrunghai.ptnk@gmail.com>
2020-05-11 13:05:40 +1200

    Menu w Play Mode Complete

---
+++ Rush Racing/Assets/Scripts/MenuScript/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+    public void FreePlayMode()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void TournamentMode()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+
+    public void GarageScene()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
