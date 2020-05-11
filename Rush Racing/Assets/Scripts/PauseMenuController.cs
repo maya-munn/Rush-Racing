01845987e0eaf8bf90af0cc2a43ba3891d1887d4
tyrelljacks <64449374+tyrelljacks@users.noreply.github.com>
2020-05-11 14:43:35 +1200

    o

---
+++ Rush Racing/Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,37 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class PauseMenuController : MonoBehaviour
+//{
+//    public static bool PausedGame = false;
+
+//    void Update()
+//    {
+//        if (Input.GetKeyDown(KeyCode.Escape))
+//        {
+//            if (PausedGame)
+//            {
+//                Resume();
+//            } else
+//            {
+//                Paused();
+//            }
+//        }         
+//    }
+
+//  //  void Resume()
+//   // {
+
+//   // }
+
+// //   void Pause()
+//  //  {
+  
+//    }
+////
+    
+
+
+
+//}
