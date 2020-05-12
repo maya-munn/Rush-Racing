456bd5215668e99926016ab4bdfea01609d0dcca
immanuelsiregar <61646253+immanuelsiregar@users.noreply.github.com>
2020-05-13 07:00:54 +1200

    Cloned master into my branch
    Cloned master into my branch

---
+++ Rush Racing/Assets/Scripts/MenuChange.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/*
+    This script allows buttons to be clicked to navigate around menus 
+    (Not including gameplay scenes for now)
+
+    Author: Bernadette Cruz
+*/
+public class MenuChange : MonoBehaviour
+{   //add default screen to start in later
+
+    public void CreateProfileMenu()
+    {
+        SceneManager.LoadScene((int)Scene.ProfileCreation);
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene((int)Scene.MainMenu);
+    }
+
+    public void Garage()
+    {   
+        SceneManager.LoadScene((int)Scene.Garage);
+    }
+
+    public void PlayMenu()
+    {
+        SceneManager.LoadScene((int)Scene.PlayMenu);
+    }
+
+    public void FreePlayMenu()
+    {
+        SceneManager.LoadScene((int)Scene.FreePlay);
+    }
+
+    public void Tournament()
+    {
+        SceneManager.LoadScene((int)Scene.Tournament);
+    }
+
+    private enum Scene
+    {
+        ProfileCreation = 0,
+        MainMenu = 1,
+        Garage = 2,
+        PlayMenu = 3,
+        FreePlay = 4,
+        Tournament = 5
+    }
+}
---
+++ Rush Racing/Assets/Scripts/WelcomeGreeting.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// This script allows for personalisation of the main menu
+/// Such as the username displayed up the top
+/// Author: Maya Ashizumi-Munn
+/// </summary>
+public class WelcomeGreeting : MonoBehaviour
+{
+    public GameObject usernameText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string username = PlayerPrefs.GetString("SessionUsername");
+        usernameText.GetComponent<TextMeshProUGUI>().text = "Hello, " + username + "!";
+    }
+}
