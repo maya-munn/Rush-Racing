ebfd8bc6bbb188257ccb2e5188945e24cb0040e7
mayamunn <mayamunn@gmail.com>
2020-05-13 09:34:58 +1200

    Revert "Merge pull request #42 from mayamunn/Bernie"
    This reverts commit 2f1be9b45730f273b39a34fe6da2fa8acca3d204, reversing
    changes made to 4153ead7e37afdcd8d7079e3180ad6fedd4ca84f.

---
+++ Rush Racing/Assets/Scripts/Database/DBSuperClass.cs
@@ -0,0 +1,33 @@
+﻿using Mono.Data.Sqlite;
+using UnityEngine;
+
+/// <summary>
+/// This script is to be used for the super class of all database scripts
+/// Initialises a connection to a db when the instance of a class is initialised
+/// 
+/// Authors: Bernadette Cruz, Maya Ashizumi-Munn
+/// </summary>
+public class DBSuperClass : MonoBehaviour
+{
+	//Connection to DB
+	protected SqliteConnection dbcon;
+
+	//******************************************//
+
+	protected SqliteConnection StartDB()
+	{
+		// Get database path
+		string connection = "URI=file:" + Application.dataPath + "/RushRacingDB.db";
+
+		// Open connection
+		dbcon = new SqliteConnection(connection);
+		dbcon.Open();
+		return dbcon;
+	}
+
+	public void closeConn()
+	{
+		// Close connection
+		dbcon.Close();
+	}
+}
