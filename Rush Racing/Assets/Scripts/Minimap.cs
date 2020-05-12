1c12535390655b9428613bb7b6b26d80d8e7f54a
Bernie Cruz <berniemaecruz@gmail.com>
2020-05-12 20:19:55 +1200

    -add minimap
    -add minimap camera
    -add gameobject 'minimap tracker' and attach to car for minimap camera to follow, in order for the camera to look ahead of the car
    -add circular minimap ui

---
+++ Rush Racing/Assets/Scripts/Minimap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*  
+* This script stabilies the minimap camera to prevent cam from flipping over if car flips over
+*/
+public class Minimap : MonoBehaviour
+{   public GameObject Car;
+    public float y;
+
+    void LateUpdate()
+    {  
+       float xe = Car.transform.position.x;
+       float ze = Car.transform.position.z;
+        y = Car.transform.eulerAngles.y;
+        transform.position = new Vector3(xe, transform.position.y, ze);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, y, 0);
+    }
+}
