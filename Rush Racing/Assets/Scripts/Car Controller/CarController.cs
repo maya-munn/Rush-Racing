f490f05bcd418e155afd6d5ad66df29753f79647
immanuelsiregar <61646253+immanuelsiregar@users.noreply.github.com>
2020-05-14 02:34:50 +1200

    Added functionality and HUD to the Lambo map.
    Added functionality and HUD to the Lambo map.

--- Rush Racing/Assets/Scripts/Car Controller/CarController.cs
+++ Rush Racing/Assets/Scripts/Car Controller/CarController.cs
@@ -28,12 +28,10 @@ public class CarController : MonoBehaviour
     public float strengthCoefficient = 20000f;  //Torque
     public float brakeStrength;                 //Strength of braking (brake torque)
     public float maxTurn = 20f;                 //Degrees
+    public float speed;
 
     public Transform centreOfMass;              //Centre of mass
     public Rigidbody rigidBody;
-
-    public Material brakePressedMat;
-    public Material normalRearLightMat;
 
     private void Start()
     {
@@ -59,11 +57,7 @@ public class CarController : MonoBehaviour
 
         foreach (GameObject tailLight in tailLights)
         {
-            //For normal cars, changing the colour will work
-            //tailLight.GetComponent<Renderer>().material.SetColor("_EmissionColor", im.brake ? new Color(0.5f, 0.111f, 0.111f) : new Color(0f, 0f, 0f));
-
-            //Lamborghini Huracan, need to change the material color directly
-            tailLight.GetComponent<MeshRenderer>().material = (im.brake ? brakePressedMat : normalRearLightMat);
+            tailLight.GetComponent<Renderer>().material.SetColor("_EmissionColor", im.brake ? new Color(0.5f, 0.111f, 0.111f) : new Color(0f, 0f, 0f));
         }
     }
 
@@ -84,46 +78,30 @@ public class CarController : MonoBehaviour
                 //If not braking, move the car
                 wheel.motorTorque = strengthCoefficient * Time.deltaTime * im.throttle;
                 wheel.brakeTorque = 0f; //If driving, do not set any brake
+                
+
             }
         }
 
         //Physics for horizontal car movement
         foreach (GameObject wheel in steeringWheels)
         {
-            ////Get the angle of turn when the wheels are steered another direction than straight
-            //wheel.GetComponent<WheelCollider>().steerAngle = maxTurn * im.steer;               
-            ////Changes the Y-axis of wheels to simulate the wheel steering in the specified direction
-            //wheel.transform.localEulerAngles = new Vector3(0f, im.steer * maxTurn, 0f);
-
-            //Custom for lamborghini
-
             //Get the angle of turn when the wheels are steered another direction than straight
-            wheel.GetComponent<WheelCollider>().steerAngle = maxTurn * im.steer;
-
+            wheel.GetComponent<WheelCollider>().steerAngle = maxTurn * im.steer;               
             //Changes the Y-axis of wheels to simulate the wheel steering in the specified direction
-            wheel.transform.localEulerAngles = new Vector3(-90f, im.steer * maxTurn, 90f);
+            wheel.transform.localEulerAngles = new Vector3(0f, im.steer * maxTurn, 0f);
         }
 
         //For each of our meshes, we need to change the rotate to simulate wheel spinning
         foreach (GameObject mesh in meshes)
         {
-            ////Math for wheel rotation accordingly to speed
-            //float magnitude = rigidBody.velocity.magnitude; //metres per second
-            //float circumference = 2 * Mathf.PI * 0.33f; //circumference of any wheel. 0.33 is default radius for wheel
-            //float direction = transform.InverseTransformDirection(rigidBody.velocity).z >= 0 ? 1 : -1; //Ternary operator to get direction of movement (backwards or forwards)
-
-            //X axis controls rotation, therefore y and z will be blank
-            //mesh.transform.Rotate(magnitude * direction / circumference, 0f, 0f);
-
-            //Custom rotation control for lamborghini huracan
-
             //Math for wheel rotation accordingly to speed
             float magnitude = rigidBody.velocity.magnitude; //metres per second
             float circumference = 2 * Mathf.PI * 0.33f; //circumference of any wheel. 0.33 is default radius for wheel
-            float direction = transform.InverseTransformDirection(rigidBody.velocity).z >= 0 ? -1 : 1; //Ternary operator to get direction of movement (backwards or forwards)
-
-            //Y axis controls rotation
-            mesh.transform.Rotate(0f, magnitude * direction / circumference, 0f);
+            float direction = transform.InverseTransformDirection(rigidBody.velocity).z >= 0 ? 1 : -1; //Ternary operator to get direction of movement (backwards or forwards)
+            speed = rigidBody.velocity.magnitude; //speed to be called from speedometer manager
+            //X axis controls rotation, therefore y and z will be blank
+            mesh.transform.Rotate(magnitude * direction / circumference, 0f, 0f); 
         }
     }
 }
