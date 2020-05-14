3755683c20de3f5d3389eea90ff879ff47a4b4d9
mayamunn <mayamunn@gmail.com>
2020-05-14 14:25:10 +1200

    Started to make scene for viewing profiles, tidying up file hierarchy
    - Unfinished scene that will be called from options menu to view, select, create and delete profiles
    - Did a little bit of moving files and folders to improve file hierarchy

--- Rush Racing/Assets/Scripts/MenuScript/MenuController.cs
+++ Rush Racing/Assets/Scripts/MenuScript/MenuController.cs
@@ -1,25 +1,48 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Contains methods to traverse through scenes and 
+/// change scenes when buttons are pressed
+/// 
+/// Collaborators: Bernie and Henry
+/// </summary>
 public class MenuController : MonoBehaviour
 {
+    private enum SceneIndex
+    {
+        MainMenu = 0,
+        FreePlay = 1,
+        Tournament = 2,
+        Garage = 3,
+        RaceScene = 4,
+        ProfileCreation = 5
+    }
+
+    //**************************//
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene((int)SceneIndex.MainMenu);
+    }
 
     public void FreePlayMode()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene((int)SceneIndex.FreePlay);
     }
 
     public void TournamentMode()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene((int)SceneIndex.Tournament);
     }
 
 
     public void GarageScene()
     {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene((int)SceneIndex.Garage);
     }
 
     public void QuitButton()
@@ -30,15 +53,40 @@ public class MenuController : MonoBehaviour
 
     public void BackToMainMenu()
     {
-        SceneManager.LoadScene(0);
+        this.MainMenu();
     }
     public void StartButton()
     {
-        SceneManager.LoadScene(4);
+        SceneManager.LoadScene((int)SceneIndex.RaceScene);
     }
 
     public void CreateProfileMenu()
     {
-        SceneManager.LoadScene(5);
+        SceneManager.LoadScene((int)SceneIndex.ProfileCreation);
+    }
+
+    /// <summary>
+    /// Allows access to options menu from any scene
+    /// 
+    /// Author: Maya Ashizumi-Munn
+    /// </summary>
+    public void OptionsMenu()
+    {
+        //Load main menu scene
+        this.MainMenu();
+        Scene mainScene = SceneManager.GetActiveScene();
+
+        //Find canvas object
+        GameObject canvasObject = GameObject.Find("Canvas");
+
+        //Find canvas menu objects
+        GameObject mainMenu = canvasObject.transform.Find("MainMenu").gameObject;
+        GameObject playMenu = canvasObject.transform.Find("PlayMenu").gameObject;
+        GameObject optionsMenu = canvasObject.transform.Find("OptionMenu").gameObject;
+
+        //Set only options menu visibility to true
+        mainMenu.SetActive(false);
+        playMenu.SetActive(false);
+        optionsMenu.SetActive(true);
     }
 }
