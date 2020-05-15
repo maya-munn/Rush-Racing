9cc03e64628a04327049b24bab20b2f51dbdf8f4
tyrelljacks <64449374+tyrelljacks@users.noreply.github.com>
2020-05-15 14:25:29 +1200

    n

---
+++ Rush Racing/Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public GameObject Pausedmenu, PauseButton;
+   
+
+    public  void Pause()
+       {
+        Pausedmenu.SetActive(true);
+        PauseButton.SetActive(false);
+        Time.timeScale = 0;
+       
+      }
+
+    public void Resume()
+    {
+        Pausedmenu.SetActive(false);
+        PauseButton.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene((int)Menuscene.MainMenu);
+        SceneManager.LoadScene(0);
+    }
+
+    private enum Menuscene
+    {
+        MainMenu = 0,
+      
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene((int)Menuscene.MainMenu);
+    }
+
+}
