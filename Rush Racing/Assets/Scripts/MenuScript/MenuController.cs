70724ad1a17391f5a4b61489cde3cc4398636bbd
bernieem <berniemaecruz@gmail.com>
2020-05-19 19:38:22 +1200

    re-add from master folder

---
+++ Rush Racing/Assets/Scripts/MenuScript/MenuController.cs
@@ -0,0 +1,92 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Contains methods to traverse through scenes and 
+/// change scenes when buttons are pressed
+/// 
+/// Collaborators: Bernie and Henry
+/// </summary>
+public class MenuController : MonoBehaviour
+{
+    private enum SceneIndex
+    {
+        MainMenu = 0,
+        FreePlay = 1,
+        Tournament = 2,
+        Garage = 3,
+        RaceScene = 4,
+        ProfileCreation = 5
+    }
+
+    //**************************//
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene((int)SceneIndex.MainMenu);
+    }
+
+    public void FreePlayMode()
+    {
+        SceneManager.LoadScene((int)SceneIndex.FreePlay);
+    }
+
+    public void TournamentMode()
+    {
+        SceneManager.LoadScene((int)SceneIndex.Tournament);
+    }
+
+
+    public void GarageScene()
+    {
+        SceneManager.LoadScene((int)SceneIndex.Garage);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+    public void BackToMainMenu()
+    {
+        this.MainMenu();
+    }
+    public void StartButton()
+    {
+        SceneManager.LoadScene((int)SceneIndex.RaceScene);
+    }
+
+    public void CreateProfileMenu()
+    {
+        SceneManager.LoadScene((int)SceneIndex.ProfileCreation);
+    }
+
+    /// <summary>
+    /// Allows access to options menu from any scene
+    /// 
+    /// Author: Maya Ashizumi-Munn
+    /// </summary>
+    public void OptionsMenu()
+    {
+        //Load main menu scene
+        this.MainMenu();
+        Scene mainScene = SceneManager.GetActiveScene();
+
+        //Find canvas object
+        GameObject canvasObject = GameObject.Find("Canvas");
+
+        //Find canvas menu objects
+        GameObject mainMenu = canvasObject.transform.Find("MainMenu").gameObject;
+        GameObject playMenu = canvasObject.transform.Find("PlayMenu").gameObject;
+        GameObject optionsMenu = canvasObject.transform.Find("OptionMenu").gameObject;
+
+        //Set only options menu visibility to true
+        mainMenu.SetActive(false);
+        playMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+}
---
+++ Rush Racing/Assets/Scripts/SpeedoManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Author: Immanuel Siregar
+//credit besnik on youtube for the sprites 
+public class SpeedoManager : MonoBehaviour
+{
+    public GameObject needle;
+
+    //The positions of the needle at 0 km/h, and 180 km/h respectively.
+    private float startPos = 220f;
+    private float endPos = -46f;
+    private float currentPos;
+    public float carSpeed;
+    public CarController RR;
+
+    // A speed variable created in the CarController script, made with rigidbody.velocity.magnitude
+    void FixedUpdate()
+    {
+        carSpeed = RR.speed;
+        updateNeedle();
+    }
+
+   
+    //How the needle will be mapped. The original speed variable will be in meters per second, so the temp variable will adjust that.
+    public void updateNeedle()
+    {
+        currentPos = startPos - endPos;
+        float temp = (carSpeed*3.6f) / 180;
+        needle.transform.eulerAngles = new Vector3(0, 0, (startPos - temp * currentPos));
+    }
+}
