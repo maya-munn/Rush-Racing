1666e596c3590ce48ecf844592db3cda25daf1ea
amasvinx <66276960+amasvinx@users.noreply.github.com>
2020-06-03 10:47:54 +1200

    garage things continue

---
+++ Rush Racing/Assets/AwakeManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AwakeManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Cars carlist;
+    public int pointer_car;
+    public GameObject Acquirebutton;
+   
+    public TextMeshProUGUI currency;
+    public TextMeshProUGUI cardetail;
+
+    private void Awake()
+    {
+        pointer_car = PlayerPrefs.GetInt("pointer");
+        currency.text = "$" + PlayerPrefs.GetInt("currency").ToString("");
+
+        GameObject childObject = Instantiate(carlist.car[pointer_car], Vector3.zero, Quaternion.identity) as GameObject;
+        childObject.tag = "player";
+        GetCarDetail();
+    }
+
+
+    public void NextCarButton()
+    {
+        Destroy(GameObject.FindGameObjectWithTag("player"));
+
+        if (pointer_car == carlist.car.Length - 1) pointer_car = 0;
+        else pointer_car++;
+
+        PlayerPrefs.SetInt("pointer", pointer_car);
+        GameObject childObject = Instantiate(carlist.car[pointer_car], Vector3.zero, Quaternion.identity) as GameObject;
+        childObject.tag = "player";
+        GetCarDetail();
+    }
+    public void PreviousCarButton()
+    {
+        Destroy(GameObject.FindGameObjectWithTag("player"));
+
+        if (pointer_car == 0) pointer_car = carlist.car.Length - 1;
+        else pointer_car--;
+
+        PlayerPrefs.SetInt("pointer", pointer_car);
+        GameObject childObject = Instantiate(carlist.car[pointer_car], Vector3.zero, Quaternion.identity) as GameObject;
+        childObject.tag = "player";
+        GetCarDetail();
+    }
+
+    public void AcquireCar()
+    {
+        carlist.car[PlayerPrefs.GetInt("pointer")].GetComponent<CarDetail>().acquire = true;
+        if (PlayerPrefs.GetInt("currency") >= carlist.car[PlayerPrefs.GetInt("pointer")].GetComponent<CarDetail>().price) {
+
+            PlayerPrefs.SetString(carlist.car[PlayerPrefs.GetInt("pointer")].GetComponent<CarDetail>().name.ToString(),
+                                    carlist.car[PlayerPrefs.GetInt("pointer")].GetComponent<CarDetail>().name.ToString());
+            PlayerPrefs.GetInt("currency", PlayerPrefs.GetInt("currency") - carlist.car[PlayerPrefs.GetInt("pointer")].GetComponent<CarDetail>().price);
+        }
+        GetCarDetail();
+        currency.text = "$" + PlayerPrefs.GetInt("currency").ToString("");
+    }
+    public void GetCarDetail()
+    {
+        if (carlist.car[PlayerPrefs.GetInt("pointer")].GetComponent<CarDetail>().name.ToString() ==
+            PlayerPrefs.GetString(carlist.car[PlayerPrefs.GetInt("pointer")].GetComponent<CarDetail>().name.ToString()))
+            {
+            Acquirebutton.SetActive(false);
+            cardetail.text = carlist.car[PlayerPrefs.GetInt("pointer")].GetComponent<CarDetail>().name.ToString();
+
+        }
+        else
+        {
+            Acquirebutton.SetActive(true);
+            cardetail.text = carlist.car[PlayerPrefs.GetInt("pointer")].GetComponent<CarDetail>().name.ToString() + carlist.car[PlayerPrefs.GetInt("pointer")].GetComponent<CarDetail>().price.ToString();
+ 
+        }
+    }
+}
---
+++ Rush Racing/Assets/CarDetail.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarDetail : MonoBehaviour
+{
+    public bool acquire;
+    public int price;
+    public string name;
+}
